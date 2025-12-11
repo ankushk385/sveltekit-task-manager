0bb5b0fc998b3e9e04907821437f8c7a302bc46e
Arun <Arun.iphone1578@gmail.com>
2025-12-11 14:41:02 +0530

    removed problomatic libary

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-  import "../app.css";
+  import "$lib/styles/app.css";
   import { page } from "$app/stores";
   import { onMount } from "svelte";
   import { goto } from "$app/navigation";
